4f6a8ea0c5ca2adc2bcdc6344d6d33a4d13890e5
Udit Desai <udit.desai@rtcamp.com>
2013-10-03 16:12:01 +0530

    Version changed

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   Plugin Name: rtwoo-gitlab
   Plugin URI: http://rtcamp.com
   Description: Gitlab Binding with Woocommerce
-  Version: 1.0.5
+  Version: 1.0.6
   Author: rtCamp
   Text Domain: rtwoo-gitlab
   Author URI: http://rtcamp.com
